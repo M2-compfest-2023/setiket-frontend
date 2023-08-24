4ee2efc125c9ea01815efb6feaa1076148452592
Fachry Anwar <fachryanwar2926@gmail.com>
2023-08-24 14:32:19 +0700

    refactor: dynamic data at my tickets page modal

--- src/pages/admin/index.page.tsx
+++ src/pages/admin/index.page.tsx
@@ -1,3 +1,4 @@
+// import withAuth from '@/components/hoc/withAuth';
 import DashboardLayout from '@/layouts/dashboard/DashboardLayout';
 import Notifications from '@/layouts/dashboard/Notifications';
 import UsersOverview from '@/layouts/dashboard/UserOverview';
