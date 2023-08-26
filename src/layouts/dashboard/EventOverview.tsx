73fc59bd0140af966fb32fcb4d3cec2d592a0119
Fachry Anwar <fachryanwar2926@gmail.com>
2023-08-26 22:51:59 +0700

    fix: fix prettier

--- src/layouts/dashboard/EventOverview.tsx
+++ src/layouts/dashboard/EventOverview.tsx
@@ -64,7 +64,7 @@ export default function EventOverview({ className }: Props) {
             >
               See Detail
             </Button>
-          </div>  
+          </div>
         );
       },
     },
